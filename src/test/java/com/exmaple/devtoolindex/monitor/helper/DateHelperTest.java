33776d03611649fd0e828638c53bdf87699bc414
hongkailiu <hongkailiu@users.noreply.github.com>
2016-11-11 19:04:55 -0500

    add a trivial test

---
+++ src/test/java/com/exmaple/devtoolindex/monitor/helper/DateHelperTest.java
@@ -0,0 +1,16 @@
+package com.exmaple.devtoolindex.monitor.helper;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hongkailiu on 2016-11-11.
+ */
+public class DateHelperTest {
+    @Test public void test() {
+        Assert.assertTrue(true);
+    }
+
+}
